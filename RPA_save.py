a44a1195622db7215ef8147e3c21e49126e25167
LucijaZuzic <42736956+LucijaZuzic@users.noreply.github.com>
2025-07-19 11:38:01 +0200

    Fix names and unit

--- RPA_save.py
+++ RPA_save.py
@@ -11,7 +11,7 @@ from pyrqa.computation import RPComputation
 import matplotlib.pyplot as plt
 from matplotlib.colors import LinearSegmentedColormap
 
-translate_variable = {"horizontal(deg)": "Horizonal error in ($\\degree$)", "Dst": "Dst"}
+translate_variable = {"horizontal(m)": "Horizonal error in [$m$])", "Dst": "$Dst$"}
 
 for used_col in translate_variable:
     for month_use in range(1, 13):
